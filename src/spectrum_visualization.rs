0ce5cfe7889d83180b515664773677441486fa6d
Kirill Shpin <kirill.shpin@gmail.com>
2022-11-27 21:07:41 -0800

    now using automatic gradient instead of manual

--- src/spectrum_visualization.rs
+++ src/spectrum_visualization.rs
@@ -1,20 +1,22 @@
 use std::iter;
 
-use iced::widget::canvas::{path, Canvas, Cursor, Frame, Geometry, Program, Stroke};
+use iced::widget::canvas::{
+    gradient::Linear, path, stroke::Style, Canvas, Cursor, Frame, Geometry, LineCap, LineDash,
+    LineJoin, Program, Stroke,
+};
 use iced::widget::Container;
 use iced::{Color, Element, Length, Rectangle, Theme};
-use iced_graphics::Point;
+use iced_graphics::gradient::ColorStop;
+use iced_graphics::{Gradient, Point};
 
+use palette::RgbHue;
 use palette::{convert::IntoColor, Hsv, Hue, Srgb};
-use palette::{RgbHue, Saturate};
 use ringbuffer::RingBufferExt;
 use spectrum_analyzer::{samples_fft_to_spectrum, windows, FrequencyLimit};
 
 use crate::sound_proxy::Clip;
 use crate::sound_transformer::SoundTransformer;
 use crate::{AppMessage, ContentType, Sides};
-
-const GRADIENT_GRANULARITY: u32 = 5;
 
 pub enum VisualizerMessage {
     SwitchDisplayContent,
@@ -150,15 +152,24 @@ impl Program<AppMessage> for Visualizer {
         bounds: Rectangle,
         _cursor: Cursor,
     ) -> Vec<Geometry> {
-        //println!("{}", bounds.size());
-
-        // We prepare a new `Frame`
-        let mut frame = Frame::new(bounds.size());
-
         // TODO: play around with colors more
         // start at green, which is brighter than red, then rotate back to red, which doesn't actually yield back red :/
         let red = Hsv::new(0f32, 1f32, 1f32);
         //let red = yellow.shift_hue(LabHue::from_degrees(-120f32));
+
+        let white = Color::from_rgb8(0xff, 0xff, 0xff);
+        let stroke = Stroke {
+            style: Style::Solid(white),
+            width: 1f32,
+            line_cap: LineCap::Square,
+            line_join: LineJoin::Bevel,
+            line_dash: LineDash {
+                segments: &[],
+                offset: 0usize,
+            },
+        };
+
+        let mut frame = Frame::new(bounds.size());
 
         match self.display_type {
             crate::DisplayType::Lines => {
@@ -168,54 +179,100 @@ impl Program<AppMessage> for Visualizer {
                 for (index, (left_val, right_val)) in both_data.enumerate() {
                     let y = (frame.height() as i32 - index as i32) as f32;
                     let color_shift = RgbHue::from_degrees(360f32 * index as f32 / frame.height());
-                    let tip_color = red.shift_hue(color_shift);
+                    let tip_color: Srgb = red.shift_hue(color_shift).into_color();
+                    let color = Color::from_rgb(tip_color.red, tip_color.green, tip_color.blue);
 
-                    let avg = if self.off_center {
-                        right_val - left_val
-                    } else {
-                        0f32
+                    let center_point = Point { x: center, y };
+                    let left_point = Point {
+                        x: center - left_val,
+                        y,
                     };
-
-                    let mut draw_side = |distance| {
-                        for i in 0..GRADIENT_GRANULARITY {
-                            let mut path_builder = path::Builder::new();
-                            path_builder.move_to(Point {
-                                x: center
-                                    * (1f32
-                                        + (avg + distance)
-                                            * (i as f32 / GRADIENT_GRANULARITY as f32)),
-                                y,
-                            });
-                            path_builder.line_to(Point {
-                                x: center
-                                    * (1f32
-                                        + (avg + distance)
-                                            * ((i as f32 + 1f32) / GRADIENT_GRANULARITY as f32)),
-                                y,
-                            });
-                            let path = path_builder.build();
-
-                            let srgb: Srgb = tip_color
-                                .desaturate_fixed(
-                                    (GRADIENT_GRANULARITY - i) as f32 / GRADIENT_GRANULARITY as f32,
-                                )
-                                .into_color();
-                            let color = Color::new(srgb.red, srgb.green, srgb.blue, 1f32);
-
-                            frame.stroke(
-                                &path,
-                                Stroke::default().with_color(color).with_width(1f32),
-                            );
-                        }
+                    let right_point = Point {
+                        x: center + right_val,
+                        y,
                     };
 
                     if self.off_center {
-                        let dist_from_middle = (right_val + left_val) / 2f32;
-                        draw_side(-dist_from_middle);
-                        draw_side(dist_from_middle);
+                        let mut path_builder = path::Builder::new();
+                        path_builder.move_to(left_point);
+                        path_builder.line_to(right_point);
+                        let path = path_builder.build();
+                        frame.stroke(
+                            &path,
+                            Stroke {
+                                style: Style::Gradient(Gradient::Linear(Linear {
+                                    start: left_point,
+                                    end: right_point,
+                                    color_stops: vec![
+                                        ColorStop {
+                                            offset: 0f32,
+                                            color,
+                                        },
+                                        ColorStop {
+                                            offset: 0.5f32,
+                                            color: white,
+                                        },
+                                        ColorStop {
+                                            offset: 1f32,
+                                            color,
+                                        },
+                                    ],
+                                })),
+                                ..stroke
+                            },
+                        );
                     } else {
-                        draw_side(-*left_val);
-                        draw_side(*right_val);
+                        // do it in two parts, easier that way
+
+                        let mut path_builder = path::Builder::new();
+                        path_builder.move_to(left_point);
+                        path_builder.line_to(center_point);
+                        let path = path_builder.build();
+                        frame.stroke(
+                            &path,
+                            Stroke {
+                                style: Style::Gradient(Gradient::Linear(Linear {
+                                    start: left_point,
+                                    end: center_point,
+                                    color_stops: vec![
+                                        ColorStop {
+                                            offset: 0f32,
+                                            color,
+                                        },
+                                        ColorStop {
+                                            offset: 1f32,
+                                            color: white,
+                                        },
+                                    ],
+                                })),
+                                ..stroke
+                            },
+                        );
+
+                        let mut path_builder = path::Builder::new();
+                        path_builder.move_to(center_point);
+                        path_builder.line_to(right_point);
+                        let path = path_builder.build();
+                        frame.stroke(
+                            &path,
+                            Stroke {
+                                style: Style::Gradient(Gradient::Linear(Linear {
+                                    start: center_point,
+                                    end: right_point,
+                                    color_stops: vec![
+                                        ColorStop {
+                                            offset: 0f32,
+                                            color: white,
+                                        },
+                                        ColorStop {
+                                            offset: 1f32,
+                                            color,
+                                        },
+                                    ],
+                                })),
+                                ..stroke
+                            },
+                        );
                     }
                 }
 
